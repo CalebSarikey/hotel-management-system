94fd5a47fd3006afc7e49a0dadab839e1e757142
CALEB SARIKEY <calebsarikey@CALEBs-MBP.home>
2018-05-24 10:27:02 -0400

    retreives and displays guest data from DB

---
+++ get-data.php
@@ -0,0 +1,103 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Index</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <script src="jquery-3.3.1.min.js"></script>
+    <script src="change-background.js"></script>
+  </head>
+  <body>
+    <div class="title-container">
+      <h1>TCWS Hotels</h1>
+    </div>
+    <div class="background-container">
+<?php
+require 'DB.php';
+
+$db = new DB();
+
+$guestFirst = $_POST['first_name'];
+$guestLast = $_POST['last_name'];
+
+
+$stmt = $db->conn->prepare("SELECT guest_id FROM GUEST WHERE first_name=? AND last_name=?");
+  if(!$stmt) die("Prepare failed: " . $db->conn->error);
+
+$bind = $stmt->bind_param("ss", $guestFirst, $guestLast);
+  if(!$bind) die("Prepare failed: " . $stmt->error);
+
+$execute = $stmt->execute();
+  if(!$execute) die("Execute failed: " . $stmt->error);
+
+$bResult = $stmt->bind_result($guestID);
+$stmt->fetch();
+
+if(!$guestID)
+{
+  header('Location: show-data.php?could-not-find-guest');
+}
+
+$stmt->free_result();
+////////////////////////////////////////////////
+
+//$statement = $db->query("SELECT * FROM RESERVE WHERE guest_id = $guestID");
+$statement = $db->query("SELECT * FROM RESERVE AS R, GUEST AS G WHERE G.GUEST_ID=R.GUEST_ID AND G.guest_id = $guestID");
+$i = 0;
+$reservations = array();
+
+while($row = $statement->fetch_assoc())
+{
+  echo 'Guest ID: ' .$row['GUEST_ID']. "<br>";
+  echo 'First Name: ' .$row['FIRST_NAME']. "<br>";
+  echo 'Last Name: ' .$row['LAST_NAME']. "<br>";
+  echo 'Room Number: ' .$row['ROOM_ID']. "<br>";
+  echo 'Check In Date: ' .$row['RES_START']. "<br>";
+  echo 'Check Out Date: ' .$row['RES_END']. "<br>";
+  echo 'Reservation ID: ' .$row['RES_ID']. "<br>";
+  $reservations[$i] = $row;
+  $i++;
+}
+
+return $reservations;
+
+if(!$result->fetch_assoc())
+{return;}
+
+//header('Location: show-data.php?guest='.$guestID);
+
+//echo "<p> Guest's First Name: " $guestFirst "</p>";
+//echo "<p> Guest's First Name: " $guestLast "</p>";
+
+//foreach($reservations as $reservation){
+  //echo 'Room Number: ' .$reservation['room_id']. "<br>";
+  //echo "<p> Check In Date: " $reservation['res_start'] "</p>";
+  //echo "<p> Check Out Date: " $reservation['res_end'] "</p>";}
+
+ ?>
+ </div>
+ <div class="table-container">
+ <div class="table">
+ <ul class="nav-bar">
+   <li><a href="make-reservation.php">Make Reservation</a></li>
+   <li><a href="check-in.php">Check In</a></li>
+   <li><a href="check-out.php">Check Out</a></li>
+   <li><a href="request-service.php">Request Room Service</a></li>
+   <li><a href="show-data.php">Show Guest Data</a></li>
+ </ul>
+ </div>
+ </div>
+ <div class="information-container">
+   <h2>Comfort and Closeness at a Price You'll Love.</h2>
+   <p>Discover a refresing hotel retreat in the heart of downtown Dunkirk, NY. With
+     stylish accommodations and invigorating amenities, TWCS Hotel will make you free right
+     at home during your stay.</p>
+     <p>Well-appointed rooms and suites boast modern decor and perks including our iconic
+     beds. Maintain your well-being at the fitness center, or explore Dunkirk on foot with
+     our running program. If you're hosting an event in town, we offer flexible venues and
+     comprehensive planning and catering services. Our hotel also provides eacy access to some
+     of the city's top attractions. Whether you're traveling for business or pleasure, TWCS
+     Hotel invites you to experience a revitalizing escape</p>
+ </div>
+ </body>
+ </html>
